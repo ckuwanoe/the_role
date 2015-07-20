5bedf06133b26d353b934d98961e914f92121345
Corey Kuwanoe <corey@politech.io>
2015-07-19 19:34:38 -0700

    changing auto load paths

--- lib/the_role.rb
+++ lib/the_role.rb
@@ -26,14 +26,16 @@ module TheRole
 
     # http://stackoverflow.com/questions/6279325/adding-to-rails-autoload-path-from-gem
     # config.to_prepare do; end
+
+    # Loading of concerns
+    config.autoload_paths << "#{_root_}/config/routes.rb"
+    config.autoload_paths << "#{_root_}/app/controllers/concerns/controller.rb"
+
   end
 end
 
 _root_ = File.expand_path('../../',  __FILE__)
 
-# Loading of concerns
-config.autoload_paths << "#{_root_}/config/routes.rb"
-config.autoload_paths << "#{_root_}/app/controllers/concerns/controller.rb"
 
 %w[ base role user ].each do |concern|
   require "#{_root_}/app/models/concerns/#{concern}.rb"
